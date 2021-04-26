dd1f7bb99747f0d02745215022eccb2e8edd2c18
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-26 13:25:56 -0400

    commit tree febf7da4b52190e956876684c336ae715942c194

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    return sourceArray.map( x => x * -1)
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map( x => x )
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map( x => x * 2 )
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map( x => x * x )
+}
+
+function reduceToTotal(array, start = 0) {
+    return array.reduce(function(accumulator, element){ return element + accumulator}, start)
+}
+
+// function reduceToTotal(array, start = 0) {
+//     return array.reduce((total, amount) => total + amount) + start
+// }
+
+function reduceToAllTrue(array) {
+    // return array.reduce((total, element) => !!element)
+    return array.every(element => !!element)
+}
+
+function reduceToAnyTrue(array) {
+    return array.some(element => !!element)
+}
+
